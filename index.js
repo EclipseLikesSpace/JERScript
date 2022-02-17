c40ed4626883e02045bd877d01a8d0a3613c4f20
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-17 16:34:37 +0800

    garbage branch

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 
 // Imports
-const chalk = import('chalk')
-const figlet = require('figlet')
+import chalk from 'chalk';
 
 // ./lib
-const lexer = require('./lib/lexer');
-const parser = require('./lib/parser');
-const generator = require('./lib/generator');
+import lexer from './lib/lexer/index.js';
+import parser from './lib/parser/index.js';
+import generator from './lib/generator/index.js';
+
+// Yargonaut styling
+import yargonaut from 'yargonaut';
+yargonaut
+  .style('blue')
+  .errorsStyle('red');
+
+import yargs from 'yargs';
 
 const fs = require('fs');
 const { parse } = require('path');
-const argv = require("yargs/yargs")(process.argv.slice(2))
+let argv = yargs(process.argv.slice(2))
   .option("file", {
     alias: "f",
     describe: "Save the HTML to disk",
@@ -20,8 +27,10 @@ const argv = require("yargs/yargs")(process.argv.slice(2))
   .help().argv;
 
 (async () => {
-    const data = ((await fs.readFileSync(argv.file, "utf8")).toString());
-    const ast = parser(data);
-
-    generator(ast);
+  console.log(chalk.blue("Reading file..."));
+  const data = ((await fs.readFileSync(argv.file, "utf8")).toString());
+  console.log(("Parsing file..."));
+  const ast = parser(data);
+  console.log(chalk.blue("Running..."));
+  generator(ast);
 })();
